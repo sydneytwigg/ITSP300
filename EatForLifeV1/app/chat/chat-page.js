5df7ffbe154123f29126fd36bdb8e313f5212c5c
Sydney <sydneytwigg@gmail.com>
2018-08-28 21:26:34 +0200

    Styling changes

---
+++ EatForLifeV1/app/chat/chat-page.js
@@ -0,0 +1,11 @@
+const ChatViewModel = require("./chat-view-model");
+
+/* ***********************************************************
+* Use the "onNavigatingTo" handler to initialize the page binding context.
+*************************************************************/
+function onNavigatingTo(args) {
+    const page = args.object;
+    page.bindingContext = new ChatViewModel();
+}
+
+exports.onNavigatingTo = onNavigatingTo;
