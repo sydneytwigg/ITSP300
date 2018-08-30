b43998fa813b78ba5f46333ed84eb4be9590c297
Sydney <sydneytwigg@gmail.com>
2018-08-30 22:18:16 +0200

    Integrated meal plan 7 day view + meal plan day view

---
+++ EatForLifeV1/app/mealplan-overview/mealplan-overview-view-model.js
@@ -0,0 +1,9 @@
+const observableModule = require("data/observable");
+
+function MealplanOverviewViewModel() {
+    const viewModel = observableModule.fromObject({});
+
+    return viewModel;
+}
+
+module.exports = MealplanOverviewViewModel;
