78917b7a5c53012ad50b353c84b7c6de85929c46
Sydney <sydneytwigg@gmail.com>
2018-08-31 10:35:08 +0200

    Added Home Screen data

--- EatForLifeV1/app/home/home-items-view-model.js
+++ EatForLifeV1/app/home/home-items-view-model.js
@@ -1,60 +1,26 @@
-const observableModule = require("data/observable");
+var observableModule = require("data/observable");
 
-function HomeItemsViewModel() {
-    const viewModel = observableModule.fromObject({
-        items: [
-            {
-                name: "Hello",
-                description: "Description for Item 1"
-            },
-            {
-                name: "Item 2",
-                description: "Description for Item 2"
-            },
-            {
-                name: "Item 3",
-                description: "Description for Item 3"
-            },
-            {
-                name: "Item 4",
-                description: "Description for Item 4"
-            },
-            {
-                name: "Item 5",
-                description: "Description for Item 5"
-            },
-            {
-                name: "Item 6",
-                description: "Description for Item 6"
-            },
-            {
-                name: "Item 7",
-                description: "Description for Item 7"
-            },
-            {
-                name: "Item 8",
-                description: "Description for Item 8"
-            },
-            {
-                name: "Item 9",
-                description: "Description for Item 9"
-            },
-            {
-                name: "Item 10",
-                description: "Description for Item 10"
-            },
-            {
-                name: "Item 11",
-                description: "Description for Item 11"
-            },
-            {
-                name: "Item 12",
-                description: "Description for Item 12"
-            }
-        ]
-    });
+function HomeViewModel() {
+  var viewModel = observableModule.fromObject({
 
-    return viewModel;
+    onItemTap: function (args) {
+      console.log('Item with index: ' + args.index + ' tapped');
+    },
+
+    categoricalSource: [
+      { Date: "12/03/2018", Weight: 96 },
+      { Date: "18/05/2018", Weight: 90 },
+      { Date: "20/06/2018", Weight: 89 },
+      { Date: "01/08/2018", Weight: 87 },
+      { Date: "28/09/2018", Weight: 86 }
+    ],
+
+    progressValue: 80,
+
+
+  });
+
+  return viewModel;
 }
 
-module.exports = HomeItemsViewModel;
+module.exports = HomeViewModel;
