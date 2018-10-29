f859fc75521a564f48b0c24737a60ddf92a64085
Justin Bruce <justinbruce34@gmail.com>
2018-10-29 21:11:53 +0200

    adding dbconnection

--- Syd-EFL-Folder/EatForLifeV1/app/Profile/Profile-view-model.js
+++ Syd-EFL-Folder/EatForLifeV1/app/Profile/Profile-view-model.js
@@ -1,7 +1,32 @@
 const observableModule = require("data/observable");
 
-function ProfileViewModel() {
+var Sqlite = require("nativescript-sqlite");
+
+function ProfileViewModel(db) {
     const viewModel = observableModule.fromObject({});
+
+    	var clients = [];
+    	viewModel.clients = [];
+    	viewModel.has_clients = false;
+   	
+   		db.all("SELECT * FROM client").then((err, rs) => {
+    if(!err){
+        rs.forEach((c) => {
+    clients.push({
+        name: commafy(c[12]),
+        surname: commafy(c[13]) 
+    });
+ 
+});
+ 
+if(clients.length){
+    viewModel.set('has_clients', true);
+}
+viewModel.set('clients', clients);
+ 
+    }  
+});
+   	
 
     return viewModel;
 }
