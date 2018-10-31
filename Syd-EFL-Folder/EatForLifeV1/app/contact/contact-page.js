aadfbe7dbad97c1a051cb127f37cc7d762f44d74
Sydney <sydneytwigg@gmail.com>
2018-10-31 19:39:54 +0200

    Had to force push all to get images.

--- Syd-EFL-Folder/EatForLifeV1/app/contact/contact-page.js
+++ Syd-EFL-Folder/EatForLifeV1/app/contact/contact-page.js
@@ -1,10 +1,20 @@
 const ContactViewModel = require("./contact-view-model");
+var view = require("ui/core/view");
 
 /* ***********************************************************
 * Use the "onNavigatingTo" handler to initialize the page binding context.
 *************************************************************/
 function onNavigatingTo(args) {
     const page = args.object;
+    var txtName = view.getViewById(page, "contactName");
+    var txtEmail = view.getViewById(page, "contactEmail");
+    var txtMessage = view.getViewById(page, "contactMessage");
+
+    sendEmail.on("tap", function () {
+      var result =="";
+      
+    });
+
     page.bindingContext = new ContactViewModel();
 }
 
---
+++ Syd-EFL-Folder/login/app/shared/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    apiUrl: "https://baas.kinvey.com/",
+    appKey: "kid_Bkiax04nm",
+    appUserHeader: "Basic a2lkX0JraWF4MDRubToyYTUwMjczYzczMDc0MjAwYTA3Zjg0MTNjZjg5NTg2ZA=="
+};
