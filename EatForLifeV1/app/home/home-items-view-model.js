4dd6b5222134879ef6c6269b62452dfb415cedb4
Justin Bruce <justinbruce34@gmail.com>
2018-11-01 15:48:08 +0200

    WORKING

--- EatForLifeV1/app/home/home-items-view-model.js
+++ EatForLifeV1/app/home/home-items-view-model.js
@@ -1,16 +1,32 @@
-var observableModule = require("data/observable");
+var frameModule = require("ui/frame");
+var Observable = require("data/observable").Observable;
+var Sqlite = require("nativescript-sqlite");
 
-function HomeViewModel() {
-  var viewModel = observableModule.fromObject({
-    categoricalSource: [
+function HomeViewModel(db) {
+  var viewModel = new Observable();
+
+
+    viewModel.categoricalSource = [
       { Date: "12/03/2018", Weight: 96 },
       { Date: "18/05/2018", Weight: 90 },
       { Date: "20/06/2018", Weight: 89 },
       { Date: "01/08/2018", Weight: 87 },
       { Date: "28/09/2018", Weight: 86 }
-    ],
-    progressValue: 80,
-  });
+    ];
+    
+    viewModel.progressValue = 80;
+
+  db.get("SELECT * FROM client").then(rows => {
+            var welcome = "Welcome, "+ rows[10];
+            viewModel.set("disName", welcome);
+            viewModel.set("name",rows[10]);
+            viewModel.set("currWeight", rows[11]);
+            viewModel.set("lostWeight",rows[12]);
+            viewModel.set("bmi",rows[13]);
+            alert("It's working");
+        }, error => {
+            alert("SELECT ERROR", error);
+        });
 
   return viewModel;
 }
