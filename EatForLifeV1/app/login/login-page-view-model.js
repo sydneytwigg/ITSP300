76760b6e731b49bbd274cf85461f9bf6cb2f08e4
Justin Bruce <justinbruce34@gmail.com>
2018-11-02 18:25:52 +0200

    Validation and db connection

--- EatForLifeV1/app/login/login-page-view-model.js
+++ EatForLifeV1/app/login/login-page-view-model.js
@@ -1,9 +1,36 @@
-var observableModule = require("tns-core-modules/data/observable");
+var Observable = require("data/observable").Observable;
+var Sqlite = require("nativescript-sqlite");
+var validator = require("email-validator");
 
-function LoginViewModel() {
-    var viewModel = observableModule.fromObject({
+function LoginViewModel(db) {
+    var viewModel = new Observable();
 
-    });
+  viewModel.LoginValidate = function () {
+
+    var emailVal = viewModel.get("email");
+    var password = viewModel.get("password");
+
+
+    if (validator.validate(emailVal) == true) {
+        if (password != null) {          
+            
+			db.get("SELECT * FROM client WHERE Email = ? AND Password = ?",[emailVal, password]).then(rows => {
+                if (rows[0] != null) {
+                    alert("Logged in");
+
+                    // Navigate to home page
+                }
+
+            });
+
+        } else {
+            alert("Enter password");
+        }
+    } else {
+        alert("Enter valid email address");
+    }
+  }
+
 
     return viewModel;
 }
