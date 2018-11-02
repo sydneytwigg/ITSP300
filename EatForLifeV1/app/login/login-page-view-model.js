893d59037e84104548fb373d97f00c00a61c3a68
Justin Bruce <justinbruce34@gmail.com>
2018-11-02 19:21:46 +0200

    navigation working

--- EatForLifeV1/app/login/login-page-view-model.js
+++ EatForLifeV1/app/login/login-page-view-model.js
@@ -1,11 +1,13 @@
 var Observable = require("data/observable").Observable;
 var Sqlite = require("nativescript-sqlite");
 var validator = require("email-validator");
+let getFrameById = require("tns-core-modules/ui/frame").getFrameById;
+var LoginPage = require("./login-page");
 
 function LoginViewModel(db) {
     var viewModel = new Observable();
 
-  viewModel.LoginValidate = function () {
+  viewModel.LoginValidate = function (args) {
 
     var emailVal = viewModel.get("email");
     var password = viewModel.get("password");
@@ -14,11 +16,12 @@ function LoginViewModel(db) {
     if (validator.validate(emailVal) == true) {
         if (password != null) {          
             
-			db.get("SELECT * FROM client WHERE Email = ? AND Password = ?",[emailVal, password]).then(rows => {
+		db.get("SELECT * FROM client WHERE Email = ? AND Password = ?",[emailVal, password]).then(rows => {
                 if (rows[0] != null) {
-                    alert("Logged in");
-
-                    // Navigate to home page
+                    alert("Login Successful");
+                    const button = args.object;
+                    const page = button.page;
+                    page.frame.navigate("./home/home-items-page");
                 }
 
             });
