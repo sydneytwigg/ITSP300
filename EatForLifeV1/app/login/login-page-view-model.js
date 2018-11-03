fce278cffa78e354e1f8c90d770caf2938681f12
Justin Bruce <justinbruce34@gmail.com>
2018-11-03 18:26:24 +0200

    Error on invalid sign-in and set global variable

--- EatForLifeV1/app/login/login-page-view-model.js
+++ EatForLifeV1/app/login/login-page-view-model.js
@@ -3,10 +3,12 @@ var Sqlite = require("nativescript-sqlite");
 var validator = require("email-validator");
 let getFrameById = require("tns-core-modules/ui/frame").getFrameById;
 var LoginPage = require("./login-page");
+var applicationSettingsModule = require("application-settings");
 
 function LoginViewModel(db) {
     var viewModel = new Observable();
 
+    applicationSettingsModule.getString("EFL","");
   viewModel.LoginValidate = function (args) {
 
     var emailVal = viewModel.get("email");
@@ -16,12 +18,16 @@ function LoginViewModel(db) {
     if (validator.validate(emailVal) == true) {
         if (password != null) {          
             
-		db.get("SELECT * FROM client WHERE Email = ? AND Password = ?",[emailVal, password]).then(rows => {
-                if (rows[0] != null) {
+			db.get("SELECT * FROM client WHERE Email = ? AND Password = ?",[emailVal, password]).then(rows => {
+                if (rows != null) {
                     alert("Login Successful");
+
                     const button = args.object;
                     const page = button.page;
+                    applicationSettingsModule.setString("EFL-user",emailVal);
                     page.frame.navigate("./home/home-items-page");
+                }else{
+                    alert("Incorrect Email or Password!");
                 }
 
             });
