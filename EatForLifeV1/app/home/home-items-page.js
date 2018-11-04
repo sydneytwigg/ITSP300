2310b4514435b9146050d1b7a6046927639a241f
Justin Bruce <justinbruce34@gmail.com>
2018-11-04 16:46:25 +0200

    goodbye random onItemTap

--- EatForLifeV1/app/home/home-items-page.js
+++ EatForLifeV1/app/home/home-items-page.js
@@ -3,7 +3,7 @@ var Sqlite = require("nativescript-sqlite");
 
 function onNavigatingTo(args) {
     var page = args.object;
-    
+
     var db_name = "eatforlife.sqlite";
 
   if (!Sqlite.exists(db_name)) {
@@ -12,24 +12,8 @@ function onNavigatingTo(args) {
     new Sqlite(db_name).then(db => {
         page.bindingContext = HomeViewModel(db);
     });
+
+
 }
 
-function onItemTap(args) {
-    const view = args.view;
-    const page = view.page;
-    const tappedItem = view.bindingContext;
-
-    page.frame.navigate({
-        moduleName: "home/home-item-detail/home-item-detail-page",
-        context: tappedItem,
-        animated: true,
-        transition: {
-            name: "slide",
-            duration: 200,
-            curve: "ease"
-        }
-    });
-}
-
-exports.onItemTap = onItemTap;
 exports.onNavigatingTo = onNavigatingTo;
