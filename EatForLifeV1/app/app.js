46c24b6bcff7986d12cd0e7145aa49d3fee444ab
Sydney <sydneytwigg@gmail.com>
2018-11-05 20:09:59 +0200

    Change css file

--- EatForLifeV1/app/app.js
+++ EatForLifeV1/app/app.js
@@ -1,6 +1,14 @@
 const application = require("application");
-application.setCssFileName("app.css");
+
+//set app.css as the css file for the project.
+application.setCssFileName("style.css");
+//check that the app is running app.css
+var fileName = application.getCssFileName();
+console.log(`fileName ${fileName}`);
+
 application.run({ moduleName: "app-root" });
+
+//test
 //application.run({moduleName: "login/login-page"});
 /*
 Do not place any code after the application has been started as it will not
