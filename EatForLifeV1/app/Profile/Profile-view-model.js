af8d20fe57a3d0579cfd5135fd514228caf6af32
Justin Bruce <justinbruce34@gmail.com>
2018-11-05 17:18:25 +0200

    fill rest of rows

--- EatForLifeV1/app/Profile/Profile-view-model.js
+++ EatForLifeV1/app/Profile/Profile-view-model.js
@@ -11,10 +11,15 @@ function ProfileViewModel(db) {
        
     var email = applicationSettingsModule.getString("EFL-user");
 
-    db.get("SELECT * FROM client WHERE Email = ?",[email]).then(rows => {
-            viewModel.set("name",rows[10]);
-            viewModel.set("surname", rows[11]);
-            viewModel.set("phoneNum",rows[12]);
+    db.get("SELECT client.Name, client.Surname, client.Email, client.PhoneNum, client.Password, overview.Height, client.Current_Weight, client.Target_Weight from client INNER JOIN overview ON overview.idOverview = client.idOverview WHERE client.Email = ? ",[email]).then(rows => {
+            viewModel.set("name",rows[0]);
+            viewModel.set("surname", rows[1]);
+            viewModel.set("email",rows[2]);
+            viewModel.set("phoneNum",rows[3]);
+            viewModel.set("pwd",rows[4]);
+            viewModel.set("height",rows[5]);
+            viewModel.set("weight",rows[6]);
+            viewModel.set("goal",rows[7]);
 
         }, error => {
             alert("SELECT ERROR", error);
