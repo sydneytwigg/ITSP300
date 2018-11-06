a74995799c098d107c4a14b50431b38c70028493
Justin Bruce <justinbruce34@gmail.com>
2018-11-07 01:17:04 +0200

    trying to fill graphs

--- EatForLifeV1/app/home/home-items-view-model.js
+++ EatForLifeV1/app/home/home-items-view-model.js
@@ -1,31 +1,67 @@
 var frameModule = require("ui/frame");
 var Observable = require("data/observable").Observable;
 var Sqlite = require("nativescript-sqlite");
+var applicationSettingsModule = require("application-settings");
 
 function HomeViewModel(db) {
   var viewModel = new Observable();
 
 
-    viewModel.categoricalSource = [
-      { Date: "12/03/2018", Weight: 96 },
-      { Date: "18/05/2018", Weight: 90 },
-      { Date: "20/06/2018", Weight: 89 },
-      { Date: "01/08/2018", Weight: 87 },
-      { Date: "28/09/2018", Weight: 86 }
-    ];
+    viewModel.categoricalSource = [];
 
-    viewModel.progressValue = 30;
+    viewModel.categoricalSourceBMI = [];
 
-  db.get("SELECT * FROM client").then(rows => {
-            var welcome = "Welcome, "+ rows[10];
+
+    var email = applicationSettingsModule.getString("EFL-user");
+
+  db.get("SELECT distinct client.Name, client.Surname, client.Current_Weight, client.Target_Weight, weight_log.BMI from client INNER JOIN weight_log ON weight_log.idClient = client.idClient WHERE client.Email = ? ",[email]).then(rows => {
+            var welcome = "Welcome, " + rows[0];
             viewModel.set("disName", welcome);
-            viewModel.set("name",rows[10]);
-            viewModel.set("currWeight", rows[11]);
-            viewModel.set("lostWeight",rows[12]);
-            viewModel.set("bmi",rows[13]);
+            viewModel.set("currWeight", rows[1]);
+           
+           var value = (parseFloat(rows[4])/parseFloat(rows[3]))*100;
+           var value2 = Math.floor(value)
+           viewModel.set("progressValue",value2);
+           var value3 = parseFloat(rows[4]);
+           var value4 = Math.floor(value3);
+            viewModel.set("bmi", String(value4));
         }, error => {
             alert("SELECT ERROR", error);
         });
+
+  var clientID = applicationSettingsModule.getString("EFL-ID");
+db.all("SELECT Date, Weight FROM weight_log WHERE idClient = ? GROUP BY Date LIMIT 10",[clientID]).then(rows => {
+
+      rows.forEach((w) => {
+        var value5 = parseFloat(w[1]);
+        var value6 = Math.floor(value5);
+        var value7 = w[0];
+            viewModel.categoricalSource.push({ 
+                  Date: value7, 
+                 Weight: value6
+                });
+
+            });
+        }, error => {
+              alert("SELECT ERROR", error);
+         });
+
+
+db.all("SELECT Date, BMI FROM weight_log WHERE idClient = ? GROUP BY Date LIMIT 10",[clientID]).then(rows => {
+
+      rows.forEach((w) => {
+        var value5 = parseFloat(w[1]);
+        var value6 = Math.floor(value5);
+        var value7 = w[0];
+            viewModel.categoricalSourceBMI.push({ 
+                  Date: value7, 
+                 Weight: value6
+                });
+
+            });
+        }, error => {
+              alert("SELECT ERROR", error);
+         });
 
   return viewModel;
 }
