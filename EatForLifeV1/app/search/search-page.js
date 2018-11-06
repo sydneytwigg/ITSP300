9d5c4d24ad0916f0bcb1b8ce0c7e2c1bbd4853fb
Sydney <sydneytwigg@gmail.com>
2018-11-06 18:21:56 +0200

    Cleanup

--- EatForLifeV1/app/search/search-page.js
+++
@@ -1,8 +0,0 @@
-const SearchViewModel = require("./search-view-model");
-
-function onNavigatingTo(args) {
-    const component = args.object;
-    component.bindingContext = new SearchViewModel();
-}
-
-exports.onNavigatingTo = onNavigatingTo;
