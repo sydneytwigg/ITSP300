cbc08bd9a736b0ef1636a8ad6b54a6be33aa9ffe
Justin Bruce <justinbruce34@gmail.com>
2018-11-06 17:51:17 +0200

    almost there

--- EatForLifeV1/app/ShoppingList/ShoppingList-view-model.js
+++ EatForLifeV1/app/ShoppingList/ShoppingList-view-model.js
@@ -1,29 +1,215 @@
-var observableModule = require("tns-core-modules/data/observable");
-var ObservableArray = require("data/observable-array").ObservableArray;
+var Observable = require("data/observable").Observable;
+var Sqlite = require("nativescript-sqlite");
+var applicationSettingsModule = require("application-settings");
+
+function ShoppinglistViewModel(db) {
+   var viewModel = new Observable();
+
+  viewModel.countries = [];
+
+    var mealPlan = applicationSettingsModule.getString("EFL-mealPlan");
+
+    db.get("SELECT idMPDays FROM client_meal_plan WHERE idMealPlan = ?",[mealPlan]).then(rows => {
+            
+            db.get("SELECT * FROM meal_plan_days WHERE idMPDays = ?",[rows[0]]).then(rows => {
+
+    var numDay1 = rows[1];
+    var numDay2 = rows[2];
+    var numDay3 = rows[3];
+    var numDay4 = rows[4];
+    var numDay5 = rows[5];
+    var numDay6 = rows[6];
+    var numDay7 = rows[7];
+
+db.execSQL("delete from itemList").then(id => {
+    db.execSQL("INSERT INTO itemList (idItem) VALUES ((SELECT idItem1 from list where idList = ? )), ((SELECT idItem2 from list where idList = ? )), ((SELECT idItem3 from list where idList = ? )), ((SELECT idItem4 from list where idList = ? )), ((SELECT idItem5 from list where idList = ? )), ((SELECT idItem6 from list where idList = ? )), ((SELECT idItem7 from list where idList = ? )), ((SELECT idItem8 from list where idList = ? )), ((SELECT idItem9 from list where idList = ? )), ((SELECT idItem10 from list where idList = ? )), ((SELECT idItem11 from list where idList = ? )), ((SELECT idItem12 from list where idList = ? )), ((SELECT idItem13 from list where idList = ? )), ((SELECT idItem14 from list where idList = ? )), ((SELECT idItem15 from list where idList = ? )), ((SELECT idItem16 from list where idList = ? )), ((SELECT idItem17 from list where idList = ? )), ((SELECT idItem18 from list where idList = ? ))",[numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1]).then(id => {
+
+        db.all("SELECT Description, SUM(Grams) FROM item INNER JOIN itemList ON itemList.idItem = item.idItem GROUP BY Description",[]).then(rows => {
 
 
-function ShoppingListViewModel() {
-  
-  var viewModel = observableModule.fromObject({
+            rows.forEach((w) => {
+            viewModel.countries.push({ 
+                  name: rows[0], 
+                 measurment: rows[1]
+                });
+
+            });
+
+            }, error => {
+            alert("BIG SELECT ERROR "+ error);
+        });
+          }, error => {
+            alert("BIG EXEC4 ERROR "+ error);
+        });
 
 
-    //data: new ObservableArray([
-    //  { pic: "res://icon", Name: "ACE Instant High Fibre", Amount: " 2.5 Kg" },
-    //  { pic: "res://icon", Name: "All Bran Flakes", Amount: "1 Kg" },
-    //  { pic: "res://icon", Name: "All Bran Instant Porridge", Amount: "1 Kg" },
-    //  { pic: "res://icon", Name: "Alphen Original Swiss Muesli", Amount: "1.5 Kg" },
-    //  { pic: "res://icon", Name: "Amasi", Amount: "200 g" },
-    //  { pic: "res://icon", Name: "Apple", Amount: "800g" },
-    //  { pic: "res://icon", Name: "Apple rings - dried", Amount: "2 pieces" },
-    //  { pic: "res://icon", Name: "Apple, Green, Raw, Unpeeled", Amount: "14" },
-    //  { pic: "res://icon", Name: "Apricot halves - dried", Amount: "7" },
-    //  { pic: "res://icon", Name: "Apricot halves - tinned", Amount: "3.5" },
+        }, error => {
+            alert("delete EXEC2 ERROR "+ error);
+        });
+      }, error => {
+            alert("DAY SELECT ERROR"+ error);
+        });
 
 
-    //])
-  });
+        }, error => {
+            alert("CLIENT MEAL SELECT ERROR" + error);
+        });
 
-  return viewModel;
+
+
+
+/*
+db.all("DROP TABLE IF exists itemList;\
+CREATE temporary TABLE itemList (idItem varchar(7));\
+INSERT INTO itemList VALUES ((SELECT idItem1 from list where idList = ?)), \
+((SELECT idItem2 from list where idList = ?)),\
+((SELECT idItem3 from list where idList = ?)),\
+((SELECT idItem4 from list where idList = ?)),\
+((SELECT idItem5 from list where idList = ?)),\
+((SELECT idItem6 from list where idList = ?)),\
+((SELECT idItem7 from list where idList = ?)),\
+((SELECT idItem8 from list where idList = ?)),\
+((SELECT idItem9 from list where idList = ?)),\
+((SELECT idItem10 from list where idList = ?)),\
+((SELECT idItem11 from list where idList = ?)),\
+((SELECT idItem12 from list where idList = ?)),\
+((SELECT idItem13 from list where idList = ?)),\
+((SELECT idItem14 from list where idList = ?)),\
+((SELECT idItem15 from list where idList = ?)),\
+((SELECT idItem16 from list where idList = ?)),\
+((SELECT idItem17 from list where idList = ?)),\
+((SELECT idItem18 from list where idList = ?));\
+\
+INSERT INTO itemList VALUES ((SELECT idItem1 from list where idList = ?)), \
+((SELECT idItem2 from list where idList = ?)),\
+((SELECT idItem3 from list where idList = ?)),\
+((SELECT idItem4 from list where idList = ?)),\
+((SELECT idItem5 from list where idList = ?)),\
+((SELECT idItem6 from list where idList = ?)),\
+((SELECT idItem7 from list where idList = ?)),\
+((SELECT idItem8 from list where idList = ?)),\
+((SELECT idItem9 from list where idList = ?)),\
+((SELECT idItem10 from list where idList = ?)),\
+((SELECT idItem11 from list where idList = ?)),\
+((SELECT idItem12 from list where idList = ?)),\
+((SELECT idItem13 from list where idList = ?)),\
+((SELECT idItem14 from list where idList = ?)),\
+((SELECT idItem15 from list where idList = ?)),\
+((SELECT idItem16 from list where idList = ?)),\
+((SELECT idItem17 from list where idList = ?)),\
+((SELECT idItem18 from list where idList = ?));\
+\
+INSERT INTO itemList VALUES ((SELECT idItem1 from list where idList = ?)), \
+((SELECT idItem2 from list where idList = ?)),\
+((SELECT idItem3 from list where idList = ?)),\
+((SELECT idItem4 from list where idList = ?)),\
+((SELECT idItem5 from list where idList = ?)),\
+((SELECT idItem6 from list where idList = ?)),\
+((SELECT idItem7 from list where idList = ?)),\
+((SELECT idItem8 from list where idList = ?)),\
+((SELECT idItem9 from list where idList = ?)),\
+((SELECT idItem10 from list where idList = ?)),\
+((SELECT idItem11 from list where idList = ?)),\
+((SELECT idItem12 from list where idList = ?)),\
+((SELECT idItem13 from list where idList = ?)),\
+((SELECT idItem14 from list where idList = ?)),\
+((SELECT idItem15 from list where idList = ?)),\
+((SELECT idItem16 from list where idList = ?)),\
+((SELECT idItem17 from list where idList = ?)),\
+((SELECT idItem18 from list where idList = ?));\
+\
+INSERT INTO itemList VALUES ((SELECT idItem1 from list where idList = ?)), \
+((SELECT idItem2 from list where idList = ?)),\
+((SELECT idItem3 from list where idList = ?)),\
+((SELECT idItem4 from list where idList = ?)),\
+((SELECT idItem5 from list where idList = ?)),\
+((SELECT idItem6 from list where idList = ?)),\
+((SELECT idItem7 from list where idList = ?)),\
+((SELECT idItem8 from list where idList = ?)),\
+((SELECT idItem9 from list where idList = ?)),\
+((SELECT idItem10 from list where idList = ?)),\
+((SELECT idItem11 from list where idList = ?)),\
+((SELECT idItem12 from list where idList = ?)),\
+((SELECT idItem13 from list where idList = ?)),\
+((SELECT idItem14 from list where idList = ?)),\
+((SELECT idItem15 from list where idList = ?)),\
+((SELECT idItem16 from list where idList = ?)),\
+((SELECT idItem17 from list where idList = ?)),\
+((SELECT idItem18 from list where idList = ?));\
+\
+INSERT INTO itemList VALUES ((SELECT idItem1 from list where idList = ?)), \
+((SELECT idItem2 from list where idList = ?)),\
+((SELECT idItem3 from list where idList = ?)),\
+((SELECT idItem4 from list where idList = ?)),\
+((SELECT idItem5 from list where idList = ?)),\
+((SELECT idItem6 from list where idList = ?)),\
+((SELECT idItem7 from list where idList = ?)),\
+((SELECT idItem8 from list where idList = ?)),\
+((SELECT idItem9 from list where idList = ?)),\
+((SELECT idItem10 from list where idList = ?)),\
+((SELECT idItem11 from list where idList = ?)),\
+((SELECT idItem12 from list where idList = ?)),\
+((SELECT idItem13 from list where idList = ?)),\
+((SELECT idItem14 from list where idList = ?)),\
+((SELECT idItem15 from list where idList = ?)),\
+((SELECT idItem16 from list where idList = ?)),\
+((SELECT idItem17 from list where idList = ?)),\
+((SELECT idItem18 from list where idList = ?));\
+\
+\
+INSERT INTO itemList VALUES ((SELECT idItem1 from list where idList = ?)), \
+((SELECT idItem2 from list where idList = ?)),\
+((SELECT idItem3 from list where idList = ?)),\
+((SELECT idItem4 from list where idList = ?)),\
+((SELECT idItem5 from list where idList = ?)),\
+((SELECT idItem6 from list where idList = ?)),\
+((SELECT idItem7 from list where idList = ?)),\
+((SELECT idItem8 from list where idList = ?)),\
+((SELECT idItem9 from list where idList = ?)),\
+((SELECT idItem10 from list where idList = ?)),\
+((SELECT idItem11 from list where idList = ?)),\
+((SELECT idItem12 from list where idList = ?)),\
+((SELECT idItem13 from list where idList = ?)),\
+((SELECT idItem14 from list where idList = ?)),\
+((SELECT idItem15 from list where idList = ?)),\
+((SELECT idItem16 from list where idList = ?)),\
+((SELECT idItem17 from list where idList = ?)),\
+((SELECT idItem18 from list where idList = ?));\
+\
+INSERT INTO itemList VALUES ((SELECT idItem1 from list where idList = ?)),\
+((SELECT idItem2 from list where idList = ?)),\
+((SELECT idItem3 from list where idList = ?)),\
+((SELECT idItem4 from list where idList = ?)),\
+((SELECT idItem5 from list where idList = ?)),\
+((SELECT idItem6 from list where idList = ?)),\
+((SELECT idItem7 from list where idList = ?)),\
+((SELECT idItem8 from list where idList = ?)),\
+((SELECT idItem9 from list where idList = ?)),\
+((SELECT idItem10 from list where idList = ?)),\
+((SELECT idItem11 from list where idList = ?)),\
+((SELECT idItem12 from list where idList = ?)),\
+((SELECT idItem13 from list where idList = ?)),\
+((SELECT idItem14 from list where idList = ?)),\
+((SELECT idItem15 from list where idList = ?)),\
+((SELECT idItem16 from list where idList = ?)),\
+((SELECT idItem17 from list where idList = ?)),\
+((SELECT idItem18 from list where idList = ?));\
+SELECT Description, SUM(Grams) FROM item INNER JOIN itemList ON itemList.idItem = item.idItem GROUP BY Description;",[numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay1, numDay2, numDay2, numDay2, numDay2, numDay2, numDay2, numDay2, numDay2, numDay2, numDay2, numDay2, numDay2, numDay2, numDay2, numDay2, numDay2, numDay2, numDay2, numDay3, numDay3, numDay3, numDay3, numDay3, numDay3, numDay3, numDay3, numDay3, numDay3, numDay3, numDay3, numDay3, numDay3, numDay3, numDay3, numDay3, numDay3, numDay4, numDay4, numDay4, numDay4, numDay4, numDay4, numDay4, numDay4, numDay4, numDay4, numDay4, numDay4, numDay4, numDay4, numDay4, numDay4, numDay4, numDay4, numDay5, numDay5, numDay5, numDay5, numDay5, numDay5, numDay5, numDay5, numDay5, numDay5, numDay5, numDay5, numDay5, numDay5, numDay5, numDay5, numDay5, numDay5, numDay6, numDay6, numDay6, numDay6, numDay6, numDay6, numDay6, numDay6, numDay6, numDay6, numDay6, numDay6, numDay6, numDay6, numDay6, numDay6, numDay6, numDay6, numDay7, numDay7, numDay7, numDay7, numDay7, numDay7, numDay7, numDay7, numDay7, numDay7, numDay7, numDay7, numDay7, numDay7, numDay7, numDay7, numDay7, numDay7]).then(rows => {
+            alert("Got here");
+            rows.forEach((w) => {
+            viewModel.countries.push({ 
+                  name: rows[0], 
+                  measurment: rows[1]
+                });
+
+            });
+            
+
+        }, error => {
+            alert("BIG SELECT ERROR", error);
+        });
+        */
+   return viewModel;
 }
-
-module.exports = ShoppingListViewModel;
+module.exports = ShoppinglistViewModel;
+ 
