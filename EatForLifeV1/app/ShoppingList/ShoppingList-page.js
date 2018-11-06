1fb76b8121b7ac6748afad124d34063f5810b567
Justin Bruce <justinbruce34@gmail.com>
2018-11-06 17:50:51 +0200

    Update ShoppingList-page.js

--- EatForLifeV1/app/ShoppingList/ShoppingList-page.js
+++ EatForLifeV1/app/ShoppingList/ShoppingList-page.js
@@ -1,18 +1,20 @@
 var frameModule = require("tns-core-modules/ui/frame");
 var ShoppinglistViewModel = require("./shoppinglist-view-model");
-
-var shoppinglistViewModel = new ShoppinglistViewModel();
+var Sqlite = require("nativescript-sqlite");
 
 function pageLoaded(args) {
+ var page = args.object;
+ var db_name = "eatforlife.sqlite";
 
-  var page = args.object;
-
-    page.bindingContext = shoppinglistViewModel;
+  if (!Sqlite.exists(db_name)) {
+    Sqlite.copyDatabase(db_name);
+  }
+    new Sqlite(db_name).then(db => {
+        page.bindingContext = ShoppinglistViewModel(db);
+    });
 }
+
 function onEdit(args) {
-    //console.log("Edit item tapped.");
-    //var btn = args.object;
-    //btn.bindingContext.set("isEditing", true);
     const button = args.object;
     const page = button.page;
     page.frame.navigate("./shareshoppinglist/shareshoppinglist-page");
