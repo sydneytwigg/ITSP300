9e48768d1f8a4fdb74eb0182459f0364a9082985
Justin Bruce <justinbruce34@gmail.com>
2018-11-07 07:50:56 +0200

    test

--- EatForLifeV1/app/ShoppingList/ShoppingList-page.js
+++ EatForLifeV1/app/ShoppingList/ShoppingList-page.js
@@ -1,17 +1,25 @@
 var frameModule = require("tns-core-modules/ui/frame");
 var ShoppinglistViewModel = require("./shoppinglist-view-model");
 var Sqlite = require("nativescript-sqlite");
+var applicationSettingsModule = require("application-settings");
+const topmost = require("tns-core-modules/ui/frame").topmost;
 
 function pageLoaded(args) {
  var page = args.object;
  var db_name = "eatforlife.sqlite";
-
+  applicationSettingsModule.setString("EFL-login","");
+  var login = applicationSettingsModule.getString("EFL-user");
+if(login != null){
   if (!Sqlite.exists(db_name)) {
     Sqlite.copyDatabase(db_name);
   }
     new Sqlite(db_name).then(db => {
-        page.bindingContext = ShoppinglistViewModel(db);
+        page.bindingContext =  ShoppinglistViewModel(db);
     });
+}else{
+  applicationSettingsModule.setString("EFL-login","./ShoppingList/ShoppingList-page");
+  topmost().navigate({ moduleName: "./login/login-page"});
+}
 }
 
 function onEdit(args) {
