b20582fb8450a24636dc09b96444705cbbf0499c
Justin Bruce <justinbruce34@gmail.com>
2018-11-07 07:44:48 +0200

    test

--- EatForLifeV1/app/breakfast/breakfast-view-model.js
+++ EatForLifeV1/app/breakfast/breakfast-view-model.js
@@ -1,22 +1,57 @@
 var observableModule = require("data/observable");
+const Observable = require("tns-core-modules/data/observable").Observable;
+var Sqlite = require("nativescript-sqlite");
+var applicationSettingsModule = require("application-settings");
+const topmost = require("tns-core-modules/ui/frame").topmost;
 
-function BreakfastViewModel() {
-    var viewModel = observableModule.fromObject({
-        listPicker1: ["Apple", "Pear", "Bananna", "Nuts", "Orange"
-        ],
-        selectedListPickerIndex: 0,
+function BreakfastViewModel(db) {
+    var viewModel = new Observable();
 
-        listPicker2: ["Apple", "Pear", "Bananna", "Nuts", "Orange"
-        ],
-        selectedListPickerIndex2: 0,
+    viewModel.listPicker1 = [];
+    viewModel.listPicker2 = [];
+    viewModel.listPicker3 = [];
+    viewModel.listPicker4 = [];
 
-        listPicker3: ["Apple", "Pear", "Bananna", "Nuts", "Orange"],
-        selectedListPickerIndex3: 0,
 
-        listPicker4: ["Apple", "Pear", "Bananna", "Nuts", "Orange"],
-        selectedListPickerIndex4: 0,
+    var listID applicationSettingsModule.getString("EFL-LISTID");
 
+    db.all("SELECT Description FROM item").then(rows => {
+        rows.forEach((w) => {
+            viewModel.listPicker1.push({w[0]});
+            viewModel.listPicker2.push({w[0]});
+            viewModel.listPicker3.push({w[0]});
+
+            });
+
+    }, error =>{
+        alert("ITEM SELECT ERROR"+error);
     });
+
+
+    viewModel.set("selectedListPickerIndex",0);
+    viewModel.set("selectedListPickerIndex2",0);
+    viewModel.set("selectedListPickerIndex3",0);
+    viewModel.set("selectedListPickerIndex4",0);
+
+
+
+    viewModel.saveChanges = function(){
+
+    var pick1 = viewModel.get("selectedListPickerIndex");
+    var pick2 = viewModel.get("selectedListPickerIndex2");
+    var pick3 = viewModel.get("selectedListPickerIndex3");
+
+        db.execSQL("UPDATE list SET idItem1 = ?, idItem2 = ?, idItem3 = ? WHERE idList = ?",[pick1,pick2,pick3,listID]).then(id => {
+
+            alert("Update Successful");
+            topmost().navigate({ moduleName: "./mealplan-dayview/mealplan-page"});
+
+    },error => {
+        alert("SAVE ERROR"+error);
+    });
+    }
+
+
 
 
     return viewModel;
