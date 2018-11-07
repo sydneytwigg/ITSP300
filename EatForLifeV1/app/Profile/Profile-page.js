272459b0c91a63f28aff71590324079f38bd8920
Justin Bruce <justinbruce34@gmail.com>
2018-11-07 07:47:02 +0200

    test

--- EatForLifeV1/app/Profile/Profile-page.js
+++ EatForLifeV1/app/Profile/Profile-page.js
@@ -1,20 +1,28 @@
 var frameModule = require("ui/frame");
 var ProfileViewModel = require("./Profile-view-model");
-
-
+const topmost = require("tns-core-modules/ui/frame").topmost;
+var applicationSettingsModule = require("application-settings");
 
 var Sqlite = require("nativescript-sqlite");
 
 function pageLoaded(args) {
   var page = args.object;
   var db_name = "eatforlife.sqlite";
-
+  
+  applicationSettingsModule.setString("EFL-login","");
+  var login = applicationSettingsModule.getString("EFL-user");
+if(login != null){
   if (!Sqlite.exists(db_name)) {
     Sqlite.copyDatabase(db_name);
   }
     new Sqlite(db_name).then(db => {
         page.bindingContext = ProfileViewModel(db);
     });
+}else{
+  applicationSettingsModule.setString("EFL-login","./Profile/Profile-page");
+  topmost().navigate({ moduleName: "./login/login-page"});
+}
+  
 	}
 
 function bmiButton(args){
